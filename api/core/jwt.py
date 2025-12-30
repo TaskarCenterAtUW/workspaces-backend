408a7c31df7d0b00761a5d1b5f08de7f2dccc4a8
Cy Rossignol <cy@rossignols.me>
2025-12-30 12:22:49 -0800

    Switch JWKS client to singleton for cert/key caching

---
+++ api/core/jwt.py
@@ -0,0 +1,33 @@
+import jwt
+
+from api.core.config import settings
+
+# Singleton JWKS client reused to take advantage of internal cert/key caching:
+_jwks_client: jwt.PyJWKClient | None = None
+
+
+def _get_jwks_client() -> jwt.PyJWKClient:
+    global _jwks_client
+
+    if _jwks_client is None:
+        _jwks_client = jwt.PyJWKClient(
+            f"{settings.TDEI_OIDC_URL}realms/{settings.TDEI_OIDC_REALM}"
+            f"/protocol/openid-connect/certs"
+        )
+
+    return _jwks_client
+
+
+def validate_and_decode_token(token: str) -> dict:
+    # TODO: use an async client like pyjwt-key-fetcher
+    signing_key = _get_jwks_client().get_signing_key_from_jwt(token)
+
+    decoded = jwt.decode_complete(
+        token,
+        key=signing_key.key,
+        algorithms=["RS256"],
+        # OIDC server does not currently differentiate tokens by audience
+        options={"verify_aud": False},
+    )
+
+    return decoded.get("payload", {})
