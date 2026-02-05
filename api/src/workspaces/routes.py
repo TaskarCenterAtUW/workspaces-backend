bb774611770f2029311d1c04dd393b9aa2815be5
Cy Rossignol <cy@rossignols.me>
2026-02-05 08:39:17 -0800

    Fix missing return statement in workspace bbox

--- api/src/workspaces/routes.py
+++ api/src/workspaces/routes.py
@@ -95,7 +95,7 @@ async def get_workspace_bbox(
         # this first query is for permissions checking
         await repository_ws.getById(current_user, workspace_id)
         bbox = await repository_osm.getWorkspaceBBox(current_user, workspace_id)
-
+        return bbox
     except Exception as e:
         logger.error(f"Failed to fetch workspace {workspace_id}: {str(e)}")
         raise
