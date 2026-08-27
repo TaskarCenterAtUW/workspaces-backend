4c98eaac0a53cf8f8b4766e7802f7aca93807b50
Rajesh Kantipudi <44539669+iamrajeshk@users.noreply.github.com>
2026-08-27 17:46:03 +0530

    fixed formatting issue

--- alembic_task/versions/c93bbbfd4279_added_flag_for_conflict_override.py
+++ alembic_task/versions/c93bbbfd4279_added_flag_for_conflict_override.py
@@ -8,8 +8,8 @@ Create Date: 2026-08-27 16:34:41.436834
 
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
